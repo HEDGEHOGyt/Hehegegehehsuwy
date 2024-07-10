2ffba5d577a64cb1beb7def04d3c885d9af1c71e
HEDGEHOGyt <121992707+HEDGEHOGyt@users.noreply.github.com>
2024-07-10 18:06:54 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function showPost(postId) {
+    document.getElementById(postId).style.display = 'block';
+}
+
+function hidePost(postId) {
+    document.getElementById(postId).style.display = 'none';
+}
+
